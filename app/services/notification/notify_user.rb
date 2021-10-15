2f19da779126071ee4fa720d02a5d5a362944bee
meritlouka <mertloka@gmail.com>
2021-10-15 03:50:12 +0200

    Refactor Service

--- app/services/notification/notify_user.rb
+++ app/services/notification/notify_user.rb
@@ -1,15 +1,15 @@
 module Notification
   class NotifyUser < ApplicationService
     def initialize
+      @notification_methods = ["EmailNotification"]
     end
 
     def call
       User.with_notifications.find_each(batch_size: 50) do |user|
-          notification_methods = ["EmailNotification"]
           if user.due_date_reminder_time.strftime('%H:%M') == Time.now.in_time_zone(user.time_zone).strftime('%H:%M')
             user.tickets.find_each(batch_size: 50) do |ticket|
               if ticket.due_date.strftime("%Y-%m-%d") == (Date.today + user.due_date_reminder_interval).strftime("%Y-%m-%d")
-                NotificationMethods::NotificationFactory.call(notification_methods, user)
+                NotificationMethods::NotificationFactory.call(@notification_methods, user)
               end
             end
           end
